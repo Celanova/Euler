ffdb23103938e54c5c54bb8bb1ed510945cc311c
Sam <thesamcyanide@gmail.com>
2014-10-16 15:51:02 -0400

    Cory pls.... check onCreate method in app\src\main\res\menu\calc_ui.xml Make it fullscreen m80

--- app/src/main/java/org/celanova/euler/CalcUI.java
+++ app/src/main/java/org/celanova/euler/CalcUI.java
@@ -15,6 +15,15 @@ public class CalcUI extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        //this.setContentView(R.layout.your_layout_name_here);
+
         setContentView(R.layout.activity_calc_ui);
 
         CalcIO.setOutputBoxes((TextView) findViewById(R.id.output),
