c8ebc6b6962366797cf63f21f8eaaf7b7e231274
Cory Lynch <cjl2625@gmail.com>
2014-10-30 12:00:00 -0400

    Made fields public in CalcData

--- app/src/main/java/org/celanova/euler/CalcData.java
+++ app/src/main/java/org/celanova/euler/CalcData.java
@@ -7,24 +7,13 @@ package org.celanova.euler;
  */
 public class CalcData
 {
-    private String contents;
-    private int type;
-
+    public String contents;
+    public int type;
     public static final int NUM = 0, OP = 1, VAR = 2, FUNC = 3, PAR = 4, CMD = 5;
 
     public CalcData(String contents, int type)
     {
         this.contents = contents; //if this line confuses you, oh well
         this.type = type;
-    }
-
-    public String getContents() //yay encapsulation
-    {
-        return contents;
-    }
-
-    public int getType()
-    {
-        return type;
     }
 }
